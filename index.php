1adfa5145c52c1d40a6b2b6b3927acf8e9e8ab50
PlayDuff <72228005+PlayDuff@users.noreply.github.com>
2022-02-03 10:28:22 +0100

    ajout

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+		<title>MediaPipe</title>
+		<style type="text/css">
+			@import url(./styles.css);
+		</style>
+	<body>
+        <div class="main">
+		    <h1>Presentation</h1>
+	</body>
+</html>
